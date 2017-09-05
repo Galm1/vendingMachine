822cf9d5d0b846a803202c41791b10fcc23c4a10
EliGarcia <garciaelisha93@yahoo.com>
2017-09-05 17:28:10 -0500

    update works

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const session = require('express-session');
 const parseurl = require('parseurl');
 const expressValidator = require('express-validator');
-const MongoClient = require('mongodb').MongoClient, assert = require('assert');
+const MongoClient = require('mongodb').MongoClient,
+  assert = require('assert');
 const url = 'mongodb://localhost:27017/vending';
 const ObjectId = require('mongodb').ObjectID;
 const express = require('express'),
   mustacheExpress = require('mustache-express'),
   bodyParser = require('body-parser'),
   sequelize = require('sequelize')
-  models = require("./models");
+models = require("./models");
 const app = express();
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
@@ -33,9 +34,10 @@ app.use(bodyParser.urlencoded({
 
 // get a list of items
 app.get('/api/customer/items', function(req, res) {
-  models.VMachine.findAll().then(function(snack){
-    res.json(
-      {"data": snack})
+  models.VMachine.findAll().then(function(snack) {
+    res.json({
+      "data": snack
+    })
   })
 })
 
@@ -54,8 +56,10 @@ app.post('/api/vendor/items', function(req, res) {
     quantity: quantityV,
     discription: discriptionV
   })
-  vmachine.save().then(function () {
-    res.json({"status":"success"});
+  vmachine.save().then(function() {
+    res.json({
+      "status": "success"
+    });
   })
 })
 
@@ -63,7 +67,7 @@ app.post('/api/vendor/items', function(req, res) {
 
 // purchase an item
 app.post('api/customer/items/:itemId/purchases', function(req, res) {
-
+  let amount = req.body.amount;
 })
 
 
@@ -83,26 +87,24 @@ app.get('/api/vendor/money', function(req, res) {
 
 // update item quantity, description, and cost
 app.put('/api/vendor/items/:itemId', function(req, res) {
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  models.VMachine.update({
+      name: req.body.name,
+      cost: req.body.cost,
+      quantity: req.body.quantity,
+      discription: req.body.discription
+    }, {
+      where: {
+        id: req.params.itemId
+      }
+    })
+    .then(function(snack) {
+      if(snack === 1){
+      res.json({
+        "data": "success"
+      })} res.json({
+        //why does this display when it is successful?
+        "data": "error"
+})})})
 
 
 
